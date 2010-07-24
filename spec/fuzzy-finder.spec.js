cd225553b83b732af02c29795bea33a1f37943d9
Matt Willhite <matthew.willhite@gmail.com>
2010-07-24 12:15:49 -0700

    Initial commit

---
+++ spec/fuzzy-finder.spec.js
@@ -0,0 +1,16 @@
+describe( "FuzzyFinder", function() {  
+  var findableTerms = ["tester", "testing", "tea_start", "tarter", "foul", "tempest", "tset"];
+  
+  it( "should return an array", function() {
+    expect( "test".fuzzyFinder(findableTerms).constructor ).toEqual( Array );
+  });
+  
+  it( "should return terms that contain the same letters (in the same order) as the target string", function() {
+    expect( "test".fuzzyFinder(findableTerms) ).toEqual( ["tester", "testing", "tea_start", "tempest"] );
+  });
+
+  it( "should NOT return terms that DO NOT contain the same letters (in the same order) as the target string", function() {
+    expect( "test".fuzzyFinder(findableTerms) ).not.toEqual( ["foul", "tset"] );
+  });
+  
+});
